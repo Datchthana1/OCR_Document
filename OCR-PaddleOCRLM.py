6e6a880b55c7f0ae15ae536fc171867a08685666
Datchthana1 <140144536+Datchthana1@users.noreply.github.com>
2026-01-04 13:30:09 +0700

    Edit PaddleOCRLM

--- OCR-PaddleOCRLM.py
+++ OCR-PaddleOCRLM.py
@@ -1,12 +1,42 @@
-from paddleocr import DocVLM
+from fastapi import FastAPI, Request, HTTPException
+from paddleocr import PaddleOCRVL
+import tempfile
+import os
 
-model = DocVLM(model_name="PP-DocBee-2B")
-results = model.predict(
-    input={
-        "pdf": "D:\OneFile\WorkOnly\AllCode\GLSWork\Typhoon_OCR\shared\input.pdf",
-    },
-    batch_size=1,
-)
-for res in results:
-    res.print()
-    res.save_to_json(f"./output/res.json")
+app = FastAPI()
+
+pipeline = PaddleOCRVL()
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
+@app.post("/ocr")
+async def ocr(request: Request):
+    data: bytes = await request.body()
+
+    if not data:
+        raise HTTPException(status_code=400, detail="Empty body")
+
+    tmp_path = None
+
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+            tmp.write(data)
+            tmp_path = tmp.name
+
+        output = pipeline.predict(input=tmp_path)
+
+        markdown_list = [res.markdown for res in output]
+        markdown_text = pipeline.concatenate_markdown_pages(markdown_list)
+
+        return {"markdown": markdown_text}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    finally:
+        if tmp_path and os.path.exists(tmp_path):
+            os.remove(tmp_path)
